a368e78b1d4c02498d555c792201de8001a06d96
MartinSugasti <martinsugasti@gmail.com>
2022-02-09 11:48:13 +0100

    Add Education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,32 +1,74 @@
 import React, { Component } from 'react';
+import EducationModal from './EducationModal'
 
 class Education extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      items: [
+        {
+          degree: 'Software Engineering',
+          school: 'Engineering Faculty, UDELAR',
+          startDate: '2018',
+          endDate: ''
+        },
+        {
+          degree: 'Industrial and Mechanical Engineering',
+          school: 'Engineering Faculty, UDELAR',
+          startDate: '2009',
+          endDate: '2015'
+        },
+        {
+          degree: 'First Certificate in English',
+          school: 'University of Cambridge',
+          startDate: '2000',
+          endDate: '2008'
+        }
+      ]
+    };
+
+    this.addItem = this.addItem.bind(this)
+    this.removeItem = this.removeItem.bind(this)
+  }
+
+  addItem(item) {
+    this.setState({
+      items: [...this.state.items, item]
+    });
+  }
+
+  removeItem(indexToRemove) {
+    this.setState({
+      items: this.state.items.filter((_, index) => index !== indexToRemove)
+    });
+  }
+
   render() {
     return (
       <div className="card-body py-0">
         <h3 className="mt-3 mb-0 text-md-left">
           <i className="fas fa-university mr-1"></i>
           Education
-          <i className="fas fa-plus-circle fa-xs ml-2"></i>
+          <i className="fas fa-plus-circle fa-xs ml-2" data-toggle="modal" data-target="#newEducationItemModal"></i>
         </h3>
 
-        <div className="row align-items-center text-md-left mt-2">
-          <p className="col-12 col-md-4 mb-0 font-weight-bold">Software Engineering<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <p className="col-12 col-md-5 mb-0">Engineering Faculty, UDELAR</p>
-          <p className="col-12 col-md-3 mb-0 font-weight-bold text-md-right">2018 - Present</p>
-        </div>
+        {this.state.items.map((item, index) => {
+          return (
+            <div key={index} id={index} className="row align-items-center text-md-left mt-2">
+              <p className="col-12 col-md-4 mb-0 font-weight-bold">
+                {item.degree}
+                <span onClick={this.removeItem.bind(this, index)}>
+                  <i className="far fa-trash-alt fa-sm ml-2"></i>
+                </span>
+              </p>
+              <p className="col-12 col-md-5 mb-0">{item.school}</p>
+              <p className="col-12 col-md-3 mb-0 font-weight-bold text-md-right">{item.startDate} - {item.endDate ? item.endDate : 'Present'}</p>
+            </div>
+          )
+        })}
 
-        <div className="row align-items-center text-md-left mt-2">
-          <p className="col-12 col-md-4 mb-0 font-weight-bold">Industrial and Mechanical Engineering<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <p className="col-12 col-md-5 mb-0">Engineering Faculty, UDELAR</p>
-          <p className="col-12 col-md-3 mb-0 font-weight-bold text-md-right">2009 - 2015</p>
-        </div>
-
-        <div className="row align-items-center text-md-left mt-2">
-          <p className="col-12 col-md-4 mb-0 font-weight-bold">First Certificate in English<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <p className="col-12 col-md-5 mb-0">University of Cambridge</p>
-          <p className="col-12 col-md-3 mb-0 font-weight-bold text-md-right">2000 - 2008</p>
-        </div>
+        <EducationModal addItem={this.addItem} />
       </div>
     );
   }
