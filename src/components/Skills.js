d71b4fe6e814862bbeb247d0e73680e1f6da44c5
Martin Sugasti <56696387+MartinSugasti@users.noreply.github.com>
2022-02-10 14:58:06 +0100

    Add Skills Component (#6)

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,40 +1,86 @@
 import React, { Component } from 'react';
+import SkillModal from './SkillModal'
 
 class Skills extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      primarySkills: ['Rails', 'React', 'Scrum'],
+      secondarySkills: ['HTML', 'CSS', 'Bootstrap', 'Javascript', 'Ruby', 'SQL', 'Git', 'Github']
+    };
+
+    this.addSkill = this.addSkill.bind(this)
+    this.removePrimarySkill = this.removePrimarySkill.bind(this)
+    this.removeSecondarySkill = this.removeSecondarySkill.bind(this)
+  }
+
+  addSkill(name, kind) {
+    if (kind === 'primary') {
+      this.setState({
+        primarySkills: [...this.state.primarySkills, name]
+      });
+    } else {
+      this.setState({
+        secondarySkills: [...this.state.secondarySkills, name]
+      });
+    }
+  }
+
+  removePrimarySkill(indexToRemove) {
+    this.setState({
+      primarySkills: this.state.primarySkills.filter((_, index) => index !== indexToRemove)
+    });
+  }
+
+  removeSecondarySkill(indexToRemove) {
+    this.setState({
+      secondarySkills: this.state.secondarySkills.filter((_, index) => index !== indexToRemove)
+    });
+  }
+
   render() {
     return (
       <div className="card-body py-0">
         <h3 className="mt-3 text-md-left">
           <i className="fas fa-star mr-1"></i>
           Skills
-          <i className="fas fa-plus-circle fa-xs ml-2"></i>
+          <i className="fas fa-plus-circle fa-xs ml-2" data-toggle="modal" data-target="#newSkillModal"></i>
         </h3>
 
         <div className="d-md-flex text-md-left mt-2">
-          <p className="mb-0 font-weight-bold">Rails<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0 font-weight-bold">React<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0 font-weight-bold">Scrum<i className="far fa-trash-alt fa-sm ml-2"></i></p>
+          {this.state.primarySkills.map((item, index) => {
+            return (
+              <p key={index} id={index} className="mb-0 font-weight-bold">
+                {item}
+                <span onClick={this.removePrimarySkill.bind(this, index)}>
+                  <i className="far fa-trash-alt fa-sm ml-2"></i>
+                </span>
+                {this.state.primarySkills.length !== index + 1 &&
+                  <span className="d-none d-md-inline mx-1">-</span>
+                }
+              </p>
+            )
+          })}
         </div>
 
         <div className="d-md-flex text-md-left mt-2">
-          <p className="mb-0">HTML<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0">CSS<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0">Bootstrap<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0">Javascript<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0">Ruby<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0">SQL<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0">Git<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-          <span className="d-none d-md-inline mx-1">-</span>
-          <p className="mb-0">Github<i className="far fa-trash-alt fa-sm ml-2"></i></p>
+          {this.state.secondarySkills.map((item, index) => {
+            return (
+              <p key={index} id={index} className="mb-0">
+                {item}
+                <span onClick={this.removeSecondarySkill.bind(this, index)}>
+                  <i className="far fa-trash-alt fa-sm ml-2"></i>
+                </span>
+                {this.state.secondarySkills.length !== index + 1 &&
+                  <span className="d-none d-md-inline mx-1">-</span>
+                }
+              </p>
+            )
+          })}
         </div>
+
+        <SkillModal addSkill={this.addSkill} />
       </div>
     );
   }
