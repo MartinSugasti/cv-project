ed1354867887a45d41daa8bae8430124666a4303
Martin Sugasti <56696387+MartinSugasti@users.noreply.github.com>
2022-02-10 14:13:01 +0100

    Add Work Experience Component (#5)

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,109 +1,114 @@
 import React, { Component } from 'react';
+import WorkExperienceModal from './WorkExperienceModal'
 
 class WorkExperience extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      items: [
+        {
+          position: 'Full Stack Developer',
+          company: 'Vairix',
+          startDate: '2022-02',
+          endDate: '',
+          location: 'Montevideo, Uruguay',
+          description: 'Since joining the team, I have worked as a full-stack developer using Ruby on Rails framework. Alongside RoR, I have gained experience with other languages and development tools such as React.'
+        },
+        {
+          position: 'Full Stack Developer',
+          company: 'EagerWorks SRL',
+          startDate: '2019-09',
+          endDate: '2022-02',
+          location: 'Montevideo, Uruguay',
+          description: 'Since joining the team, I have worked as a full-stack developer using Ruby on Rails framework. Alongside RoR, I have gained experience with other languages and development tools such as HTML, CSS, JS, git, GitHub, and SQL. During this time, I have dealt with a variety of clients, spanning from small local companies to large multinational corporations using Agile/Scrum development techniques, staying in touch with clients daily, and reaching mutual agreements on ideas and next steps.'
+        },
+        {
+          position: 'Maintenance Analyst',
+          company: 'Montevideo Refrescos SRL',
+          startDate: '2017-04',
+          endDate: '2019-10',
+          location: 'Montevideo, Uruguay',
+          description: 'Planned preventive and corrective maintenance for the Coca-Cola products bottling company in Uruguay. Coordinated availability of resources such as specialized technicians and spare parts with different divisions.- Led a team of 15 operators in a periodic manner.- Executed the maintenance management migration in SAP when the company ownership changed from The Coca-Cola Company to FEMSA. At this point, approximately 1,800 maintenance plans were redesigned.'
+        },
+        {
+          position: 'Production Coordinator',
+          company: 'Yazaki Uruguay SA',
+          startDate: '2015-05',
+          endDate: '2016-12',
+          location: 'Las Piedras, Uruguay',
+          description: 'Supervised production plan on a daily basis, coordinating activities with 3 production supervisors. Provided production capacity insights and information as inputs for the company\'s production strategy.- Led a team of 200 plant operators with 4 plant supervisors direct reports.- Negotiated with labor unions in weekly bi-lateral meetings.'
+        },
+        {
+          position: 'Technician Assistant',
+          company: 'Abengoa Teyma SA',
+          startDate: '2012-11',
+          endDate: '2013-10',
+          location: 'Montevideo, Uruguay',
+          description: 'Constructed the electro-mechanical cost-estimate of water purification plant and of concrete/cement plant extension.'
+        }
+      ]
+    };
+
+    this.addItem = this.addItem.bind(this)
+    this.removeItem = this.removeItem.bind(this)
+  }
+
+  addItem(item) {
+    console.log(item)
+    this.setState({
+      items: [...this.state.items, item]
+    });
+  }
+
+  removeItem(indexToRemove) {
+    this.setState({
+      items: this.state.items.filter((_, index) => index !== indexToRemove)
+    });
+  }
+
+  formatDate(date) {
+    const yearMonth = date.split('-')
+    return `${parseInt(yearMonth[1], 10)}/${yearMonth[0]}`
+  }
+
   render() {
     return (
       <div className="card-body py-0">
         <h3 className="mt-3 mb-0 text-md-left">
-          <i className="fas fa-hard-hat mr-1"></i>
-          Work Experience
-          <i className="fas fa-plus-circle fa-xs ml-2"></i>
+          <i className="fas fa-university mr-1"></i>
+            Work Experience
+          <i className="fas fa-plus-circle fa-xs ml-2" data-toggle="modal" data-target="#newWorkExperienceItemModal"></i>
         </h3>
 
-        <div className="text-md-left mt-2">
-          <div className="row">
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold">Full Stack Developer<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-              <p className="mb-0 font-weight-bold">Vairix</p>
+        {this.state.items.map((item, index) => {
+          return (
+            <div key={index} id={index} className="text-md-left mt-2">
+              <div className="row">
+                <div className="col-12 col-md-6 align-items-center">
+                  <p className="mb-0 font-weight-bold">
+                    {item.position}
+                    <span onClick={this.removeItem.bind(this, index)}>
+                      <i className="far fa-trash-alt fa-sm ml-2"></i>
+                    </span>
+                  </p>
+                  <p className="mb-0 font-weight-bold">{item.company}</p>
+                </div>
+
+                <div className="col-12 col-md-6 align-items-center">
+                  <p className="mb-0 font-weight-bold text-md-right">{this.formatDate(item.startDate)} - {item.endDate ? this.formatDate(item.endDate) : 'Present'}</p>
+                  <p className="mb-0 font-weight-bold text-md-right">{item.location}</p>
+                </div>
+              </div>
+
+              <p className="mb-0">
+                {item.description}
+              </p>
             </div>
+          )
+        })}
 
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold text-md-right">2/2021 - Present</p>
-              <p className="mb-0 font-weight-bold text-md-right">Montevideo, Uruguay</p>
-            </div>
-          </div>
-
-          <p className="mb-0">
-            Since joining the team, I have worked as a full-stack developer using Ruby on Rails framework. Alongside RoR, I have gained experience with other languages and development tools such as React.
-          </p>
-        </div>
-
-        <div className="text-md-left mt-2">
-          <div className="row">
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold">Full Stack Developer<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-              <p className="mb-0 font-weight-bold">EagerWorks SRL</p>
-            </div>
-
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold text-md-right">9/2019 - 2/2021</p>
-              <p className="mb-0 font-weight-bold text-md-right">Montevideo, Uruguay</p>
-            </div>
-          </div>
-
-          <p className="mb-0">
-            Since joining the team, I have worked as a full-stack developer using Ruby on Rails framework. Alongside RoR, I have gained experience with other languages and development tools such as HTML, CSS, JS, git, GitHub, and SQL.
-            During this time, I have dealt with a variety of clients, spanning from small local companies to large multinational corporations using Agile/Scrum development techniques, staying in touch with clients daily, and reaching mutual agreements on ideas and next steps.
-          </p>
-        </div>
-
-        <div className="text-md-left mt-2">
-          <div className="row">
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold">Maintenance Analyst<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-              <p className="mb-0 font-weight-bold">Montevideo Refrescos SRL</p>
-            </div>
-
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold text-md-right">4/2017 - 10/2019</p>
-              <p className="mb-0 font-weight-bold text-md-right">Montevideo, Uruguay</p>
-            </div>
-          </div>
-
-          <p className="mb-0">
-            Planned preventive and corrective maintenance for the Coca-Cola products bottling company in Uruguay. Coordinated availability of resources such as specialized technicians and spare parts with different divisions.<br/>
-            - Led a team of 15 operators in a periodic manner.<br/>
-            - Executed the maintenance management migration in SAP when the company ownership changed from The Coca-Cola Company to FEMSA. At this point, approximately 1,800 maintenance plans were redesigned.
-          </p>
-        </div>
-
-        <div className="text-md-left mt-2">
-          <div className="row">
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold">Production Coordinator<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-              <p className="mb-0 font-weight-bold">Yazaki Uruguay SA</p>
-            </div>
-
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold text-md-right">5/2015 - 12/2016</p>
-              <p className="mb-0 font-weight-bold text-md-right">Las Piedras, Uruguay</p>
-            </div>
-          </div>
-
-          <p className="mb-0">
-            Supervised production plan on a daily basis, coordinating activities with 3 production supervisors. Provided production capacity insights and information as inputs for the company's production strategy.<br/>
-            - Led a team of 200 plant operators with 4 plant supervisors direct reports.<br/>
-            - Negotiated with labor unions in weekly bi-lateral meetings.
-          </p>
-        </div>
-
-        <div className="text-md-left mt-2">
-          <div className="row">
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold">Technician Assistant<i className="far fa-trash-alt fa-sm ml-2"></i></p>
-              <p className="mb-0 font-weight-bold">Abengoa Teyma SA</p>
-            </div>
-
-            <div className="col-12 col-md-6 align-items-center">
-              <p className="mb-0 font-weight-bold text-md-right">11/2012 - 10/2013</p>
-              <p className="mb-0 font-weight-bold text-md-right">Montevideo, Uruguay</p>
-            </div>
-          </div>
-
-          <p className="mb-0">
-            Constructed the electro-mechanical cost-estimate of water purification plant and of concrete/cement plant extension.
-          </p>
-        </div>
+        <WorkExperienceModal addItem={this.addItem} />
       </div>
     );
   }
---
+++ src/components/WorkExperienceModal.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+
+class WorkExperienceModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      position: '',
+      company: '',
+      startDate: '',
+      endDate: '',
+      location: '',
+      description: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(addItem, event) {
+    event.preventDefault();
+
+    // create item and add it to the items list
+    const newItem = {
+      position: document.getElementById('new-work-experience-item-position').value,
+      company: document.getElementById('new-work-experience-item-company').value,
+      startDate: document.getElementById('new-work-experience-item-start-date').value,
+      endDate: document.getElementById('new-work-experience-item-end-date').value,
+      location: document.getElementById('new-work-experience-item-location').value,
+      description: document.getElementById('new-work-experience-item-description').value
+    }
+    addItem(newItem)
+
+    // next is to clean form
+    document.getElementById('new-work-experience-item-position').value = ''
+    document.getElementById('new-work-experience-item-company').value = ''
+    document.getElementById('new-work-experience-item-start-date').value = ''
+    document.getElementById('new-work-experience-item-end-date').value = ''
+    document.getElementById('new-work-experience-item-location').value = ''
+    document.getElementById('new-work-experience-item-description').value = ''
+
+    // next is to hide modal
+    const modal = document.getElementById('newWorkExperienceItemModal');
+    modal.classList.remove('show');
+    modal.setAttribute('aria-hidden', 'true');
+    modal.setAttribute('style', 'display: none');
+    const modalsBackdrops = document.getElementsByClassName('modal-backdrop');
+    for (let i = 0; i < modalsBackdrops.length; i += 1) {
+      document.body.removeChild(modalsBackdrops[i]);
+    }
+    document.body.classList.remove('modal-open');
+  }
+
+  render() {
+    return (
+      <div className="modal fade" id="newWorkExperienceItemModal" tabIndex="-1" aria-labelledby="newWorkExperienceItem" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content text-dark">
+            <form id="newWorkExperienceItemForm" onSubmit={this.handleSubmit.bind(this, this.props.addItem)}>
+              <div className="modal-header text-center">
+                <h3 className="modal-title w-100">Work Experience</h3>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+
+              <div className="modal-body pb-0">
+                <div className="form-group row">
+                  <label htmlFor="new-work-experience-item-position" className="col-form-label col-12 col-sm-4 text-sm-right mr-2">Position:</label>
+                  <div className="col-12 col-sm-6">
+                    <input type="text" className="form-control" id="new-work-experience-item-position" required="required" />
+                  </div>
+                </div>
+
+                <div className="form-group row">
+                  <label htmlFor="new-work-experience-item-company" className="col-form-label col-12 col-sm-4 text-sm-right mr-2">Company:</label>
+                  <div className="col-12 col-sm-6">
+                    <input type="text" className="form-control" id="new-work-experience-item-company" required="required" />
+                  </div>
+                </div>
+
+                <div className="form-group row">
+                  <label htmlFor="new-work-experience-item-start-date" className="col-form-label col-12 col-sm-4 text-sm-right mr-2">Start:</label>
+                  <div className="col-12 col-sm-6">
+                    <input type="month" className="form-control" id="new-work-experience-item-start-date" required="required" />
+                  </div>
+                </div>
+
+                <div className="form-group row">
+                  <label htmlFor="new-work-experience-item-end-date" className="col-form-label col-12 col-sm-4 text-sm-right mr-2">End:</label>
+                  <div className="col-12 col-sm-6">
+                    <input type="month" className="form-control" id="new-work-experience-item-end-date" />
+                  </div>
+                </div>
+
+                <div className="form-group row">
+                  <label htmlFor="new-work-experience-item-location" className="col-form-label col-12 col-sm-4 text-sm-right mr-2">Location:</label>
+                  <div className="col-12 col-sm-6">
+                    <input type="text" className="form-control" id="new-work-experience-item-location" required="required" />
+                  </div>
+                </div>
+
+                <div className="form-group row">
+                  <label htmlFor="new-work-experience-item-description" className="col-form-label col-12 col-sm-4 text-sm-right mr-2">Description:</label>
+                  <div className="col-12 col-sm-6">
+                    <textarea className="form-control" id="new-work-experience-item-description" rows="5" required="required" />
+                  </div>
+                </div>
+              </div>
+
+              <div className="modal-footer justify-content-center">
+                <button type="submit" className="btn btn-dark">Add</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WorkExperienceModal;
